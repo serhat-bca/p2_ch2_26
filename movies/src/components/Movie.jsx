15ea5c21df5eb7f63743da0618e2ba88e2882910
serhat-bca <sersen@bergen.org>
2026-05-12 09:52:52 -0400

    add remove from watchlist buttons added.

--- movies/src/components/Movie.jsx
+++ movies/src/components/Movie.jsx
@@ -1,6 +1,12 @@
 const Movie = ({ movie }) => (
   <li>
     {movie.title} - Watchlist ({movie.watchlist ? "Yes" : "No"})
+    <span
+      style={{ fontSize: "0.7rem", color: "blue", cursor: "pointer" }}
+      onClick={() => console.log("Clicked")}
+    >
+      {movie.watchlist ? " Remove" : " Add"}
+    </span>
   </li>
 );
 export default Movie;
